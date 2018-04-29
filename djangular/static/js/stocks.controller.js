f587bc7f42658d9a187f4a7aedba950eb208fb5e
Babics Bence <bbence14@gmail.com>
2018-04-29 19:50:13 +0200

    Some design changes. Login implemented into the main page.

--- djangular/static/js/stocks.controller.js
+++ djangular/static/js/stocks.controller.js
@@ -9,26 +9,40 @@
                     ['$scope', '$http', '$location', '$routeParams', 'Login', ScrumboardController]);
 
     function ScrumboardController($scope, $http, $location, $routeParams, Login) {
-        $scope.home = "Bence";
-        $scope.next_day = "Next Days";
-        $http.get('/stocks/GE/')
-            .then(function(response){
-                $scope.GE_price = response.data;
-                console.log($scope.GE_price);
-                    var ge_opens = [];
-                    var ge_dates = [];
-                    for(var i=0; i < $scope.GE_price.length; i++) {
-                        ge_opens.push($scope.GE_price[i].open_price);
-                        ge_dates.push($scope.GE_price[i].dates);
-                    }
-                    console.log(ge_opens);
-                $scope.ge_prices = ge_opens;
-                $scope.dates =ge_dates;
-            });
+        activate();
+
+        function activate(){
+            if (!Login.isLoggedIn()) {
+                $location.url('/login');
+            }
+
+            $scope.user = Login.currentUser();
+            $scope.next_day = "Next Day";
+            $http.get('/stocks/GE/')
+                .then(function(response){
+                    $scope.GE_price = response.data;
+                    console.log($scope.GE_price);
+                        var ge_opens = [];
+                        var ge_dates = [];
+                        for(var i=0; i < $scope.GE_price.length; i++) {
+                            ge_opens.push($scope.GE_price[i].open_price);
+                            ge_dates.push($scope.GE_price[i].dates);
+                        }
+                        console.log(ge_opens);
+                    $scope.ge_prices = ge_opens;
+                    $scope.dates =ge_dates;
+                });
+
+            $scope.predicted_price = 69;
 
         $scope.message = function message(){
             console.log("Clicked the button");
         }
+
+
+        }
+
+
 
 
 
